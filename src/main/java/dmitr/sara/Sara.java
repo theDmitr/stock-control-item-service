60a571339aaace86af0cfc92b1945d1a2971fe3b
theDmitr <dmitryfirson@gmail.com>
2025-02-02 18:57:21 +0300

    initial

---
+++ src/main/java/dmitr/sara/Sara.java
@@ -0,0 +1,12 @@
+package dmitr.sara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sara {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sara.class, args);
+    }
+}
